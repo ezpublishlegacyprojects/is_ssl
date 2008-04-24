42c0c91dacc8da9dd590051ec614963adaaf54aa
Björn Dieding <bjoern@xrow.de>
2008-04-24 10:57:48 +0000

    - add project
    git-svn-id: http://svn.projects.ez.no/is_ssl@1 163464e2-9dc5-475f-b145-4ec28e3bb5d9

---
+++ stable/1.0/extension/is_ssl/autoloads/eztemplateautoload.php
@@ -0,0 +1,7 @@
+<?php 
+
+$eZTemplateOperatorArray = array();
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/is_ssl/autoloads/ezis_ssloperator.php',
+                                    'class' => 'eZIsSSLOperator',
+                                    'operator_names' => array( 'is_ssl' ) );
+?>
---
+++ stable/1.0/extension/is_ssl/settings/site.ini.append.php
@@ -0,0 +1,6 @@
+<?php /*
+
+[TemplateSettings]
+ExtensionAutoloadPath[]=is_ssl
+
+*/ ?>
